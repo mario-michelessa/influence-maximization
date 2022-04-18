9627fe79e6b277ab3a968fdb83858e23a103ef69
Mario Michelessa <44181312+ajoutemoi@users.noreply.github.com>
2022-04-18 10:31:56 +0800

    dropout, wandb, hidden_sizes

--- decision_focused_learning_gpu/predictive_model_gpu.py
+++ decision_focused_learning_gpu/predictive_model_gpu.py
@@ -3,8 +3,10 @@ import torch
 import torch.nn as nn
 
 
-def make_fc(num_features, num_layers, activation, intermediate_size,):
-
+def make_fc(num_features, num_layers, activation, hidden_layers_sizes, dropout = 0):
+    """
+    hidden_layers size : list of int of length num_layers-1
+    """
     if num_layers > 1:
         if activation == 'relu':
             activation_fn = nn.ReLU
@@ -12,13 +14,15 @@ def make_fc(num_features, num_layers, activation, intermediate_size,):
             activation_fn = nn.Sigmoid
         else:
             raise Exception('Invalid activation function: ' + str(activation))
-        net_layers = [nn.Linear(num_features, intermediate_size), activation_fn()]
+        net_layers = [nn.Linear(num_features, hidden_layers_sizes[0]), activation_fn()]
         for hidden in range(num_layers-2):
-            net_layers.append(nn.Linear(intermediate_size, intermediate_size))
+            net_layers.append(nn.Linear(hidden_layers_sizes[hidden], hidden_layers_sizes[hidden + 1]))
             net_layers.append(activation_fn())
-        net_layers.append(nn.Linear(intermediate_size, 1))
+            net_layers.append(nn.Dropout(dropout))
+        net_layers.append(nn.Linear(hidden_layers_sizes[-1], 1))
         #net_layers.append(nn.ReLU())
         net_layers.append(nn.Hardtanh(min_val = 0, max_val = 1.0))
+        net_layers.append(nn.Dropout(dropout))
         return nn.Sequential(*net_layers)
     else:
         return nn.Sequential(nn.Linear(num_features, 1), nn.Hardtanh(min_val = 0, max_val = 1.0))
