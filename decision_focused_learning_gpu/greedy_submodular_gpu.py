4c1f3f975632163ea4a4a9793de06db09dad1d5b
Mario Michelessa <44181312+ajoutemoi@users.noreply.github.com>
2022-06-14 21:25:11 +0800

    first models noCas

--- decision_focused_learning_gpu/greedy_submodular_gpu.py
+++ decision_focused_learning_gpu/greedy_submodular_gpu.py
@@ -1,5 +1,6 @@
 
 import torch
+import random
 
 class GreedyOptimizer(torch.autograd.Function):
     # @staticmethod
@@ -73,3 +74,50 @@ class GreedyOptimizer(torch.autograd.Function):
     def backward(ctx, grad_output):
         grad = ctx.saved_tensors[0]
         return - grad
+
+class StochasticGreedyOptimizer(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, P, true_set_func, partial_marginal_vec, n,  K,  eps, sample_size, beta, nk):
+        vals = torch.zeros(sample_size)
+        sumlogps = torch.zeros_like(vals)
+        P.retain_grad()
+        with torch.enable_grad():
+            for i in range(sample_size): #sample size controls the number of time we execute the diff greedy algorithm
+                '''
+                perturbed greedy with gradient computation for backward
+                '''
+                S, sumlogp = [], 0
+                U = torch.tensor(range(n))
+                for k in range(K):
+                    
+                    sample_idxs = random.sample(range(len(U)), min(nk, len(U)))
+                    sample_idxs = torch.tensor(sample_idxs)
+                    Uk = U[sample_idxs]
+
+                    g = partial_marginal_vec(S, Uk, P) #only contains marginal gains of Uk
+                    if len(U) == 0: break
+                    p = torch.nn.functional.softmax(g[U]/eps) 
+                    id_v = torch.multinomial(p, num_samples=1, replacement=True) #returns the position in p
+                    v = sample_idxs[id_v] #converts it into position in U
+                    S +=[int(U[v])] 
+                    U = torch.cat([U[:v],U[v+1:]])
+
+                    # vidx = int(torch.nonzero(U == v)[0])
+                    # U = torch.cat([U[0:vidx], U[vidx+1:]])
+                    # S += [int(v)]
+                    
+                    sumlogp = sumlogp + torch.log(p[v])
+                sumlogps[i] = sumlogp
+                vals[i] = true_set_func(S)
+            beta = vals.mean() if beta else beta
+            fsumlogp = torch.dot(vals - beta, sumlogps) / sample_size
+            grad = torch.autograd.grad(fsumlogp, P, retain_graph=True)[0]
+            ctx.save_for_backward(grad)
+        return vals.mean()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad = ctx.saved_tensors[0]
+        return - grad
+
--- decision_focused_learning_gpu/greedy_submodular_new.py
+++ decision_focused_learning_gpu/greedy_submodular_new.py
@@ -1,6 +1,7 @@
 
 import torch
 from torch.autograd.function import once_differentiable
+import random
 
 #import numpy as np
 class GreedyOptimizer(torch.autograd.Function):
@@ -46,3 +47,46 @@ class GreedyOptimizer(torch.autograd.Function):
     def backward(ctx, grad_output):
         grad_P = ctx.saved_tensors[0]
         return - grad_P, None, None, None, None, None,None, None
+
+class StochasticGreedyOptimizer(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, P, true_set_func, partial_marginal_vec, n,  K,  eps, sample_size, beta, nk):
+        vals = torch.zeros(sample_size)
+        sumlogps = torch.zeros_like(vals)
+        P.retain_grad()
+        with torch.enable_grad():
+            for i in range(sample_size): #sample size controls the number of time we execute the diff greedy algorithm
+
+                S, sumlogp = [], 0
+                U = torch.tensor(range(n))
+                for k in range(K):
+                    
+                    sample_idxs = random.sample(range(len(U)), min(nk, len(U)))
+                    sample_idxs = torch.tensor(sample_idxs)
+                    Uk = U[sample_idxs]
+                    # print(sample_idxs, Uk)
+                    gvec = partial_marginal_vec(S, Uk, P) 
+                    # print(len(gvec))
+                    if len(U) == 0: break
+                    p = torch.nn.functional.softmax(gvec/eps) 
+                    id_v = torch.multinomial(p, num_samples=1, replacement=True) #returns the position in p
+                    v = sample_idxs[id_v] #converts it into position in U
+                    S +=[int(U[v])] 
+                    U = torch.cat([U[:v],U[v+1:]])
+                    sumlogp = sumlogp + torch.log(p[id_v])
+
+                sumlogps[i] = sumlogp
+                vals[i] = true_set_func(S)
+
+            beta = vals.mean() if beta else beta
+            fsumlogp = torch.dot(vals - beta, sumlogps) / sample_size
+            grad = torch.autograd.grad(fsumlogp, P, retain_graph=True)[0]
+            ctx.save_for_backward(grad)
+        return vals.mean()
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        grad = ctx.saved_tensors[0]
+        return - grad, None, None, None, None, None,None, None, None
