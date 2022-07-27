1273c567a18bf8e4f32dff86521d7eae9b9a2d30
Mario Michelessa <44181312+ajoutemoi@users.noreply.github.com>
2022-07-27 10:57:42 +0800

    generate results

--- decision_focused_learning_gpu/convert_result_file.py
+++ decision_focused_learning_gpu/convert_result_file.py
@@ -1,5 +1,5 @@
 """
-Extracts mean / std tables from the results file returned by results.py and grd_main.py
+Extracts mean / std tables from the results file returned by results.py and grd_main.py --> perfs_train_test.txt / perfs_sparse.txt
 """
 
 import numpy as np
@@ -17,10 +17,10 @@ file_path = args.file_path
 sparse = bool(args.sparse)
 
 names_sparse = ["Exp df", "Exp 2s", "Exp rnd", "Exp grd", "Exp deg", "DNI df", "DNI 2s", "DNI rnd", "DNI grd",  "DNI deg"]
-names_train_test = ["Exp-train df" , "Exp-test df" , "Exp-train 2s" , "Exp-test 2s" , 
-                    "Exp-train rnd", "Exp-test rnd", "Exp-train grd", "Exp-test grd","Exp-train deg", "Exp-test deg",
-                    "DNI-train df" , "DNI-test df" , "DNI-train 2s" , "DNI-test 2s" ,
-                    "DNI-train rnd", "DNI-test rnd", "DNI-train grd", "DNI-test grd", "DNI-train deg", "DNI-test deg"]
+names_train_test = ["Exp-train df",  "Exp-train 2s", "Exp-train rnd",  "Exp-train grd", "Exp-train deg", 
+                    "DNI-train df",  "DNI-train 2s", "DNI-train rnd",  "DNI-train grd",  "DNI-train deg",
+                    "Exp-test df", "Exp-test 2s", "Exp-test rnd", "Exp-test grd", "Exp-test deg", 
+                    "DNI-test df", "DNI-test 2s", "DNI-test rnd", "DNI-test grd", "DNI-test deg" ]
 
 with open(file_path, 'r') as file : 
     names = names_sparse if sparse else names_train_test
@@ -41,7 +41,8 @@ for k in d.keys() :
 
 df_mean = pd.DataFrame.from_dict(d_mean, orient='index')
 df_std = pd.DataFrame.from_dict(d_std, orient='index')
-
+print(df_mean.to_markdown())
+print(df_std.to_markdown())
 df_mean.to_csv(file_path.replace('.txt', '_mean.csv'))
 df_std.to_csv(file_path.replace('.txt', '_std.csv'))
 
